eee2517e47dfd3e69882ff14f3f05ecd772e0fa8
Nikita Malov <120462198+NikitaMalov@users.noreply.github.com>
2023-01-08 13:52:32 +0300

    Add files via upload

---
+++ MyNewCalculation.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class MyNewCalculation extends MyCalculation {
+
+	public void Division(int x, int y) {
+		z = x / y;
+		System.out.println("The quotient of the given numbers: " +z);
+	
+}
+}
