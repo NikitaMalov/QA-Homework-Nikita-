07a55983c7d22f218fcbda152e9d8523a73474f5
Nikita Malov <120462198+NikitaMalov@users.noreply.github.com>
2023-01-20 17:58:58 +0300

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,12 @@
+package utility;
+
+public class Constant {
+	
+	public static final String URL = "https://www.worldtimebuddy.com/";
+
+	public static final String Path_TestData = "D:\\БАЗА\\САМОРАЗВИТИЕ\\QA\\Part 2 Automation Testing\\Test environment\\AutomationWorkSpace\\QAEvolutionV4\\src\\testData/";
+	
+	public static final String File_TestData = "TestData.xlsx";
+	
+	public static final String Sheet_TestData = "Sheet1";
+}
